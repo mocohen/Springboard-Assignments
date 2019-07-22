627ed7b1763807a734a48f89e248680ba38b92fe
mocohen <mocohen@uchicago.edu>
2019-07-22 15:07:43 -0700

    Finished sql assignment

--- sql-practice/solutions.sql
+++ sql-practice/solutions.sql
@@ -48,8 +48,7 @@ who signed up. Do not use the LIMIT clause for your solution. */
 SELECT surname, firstname
 FROM Members
 WHERE joindate = ( SELECT MAX( joindate ) 
-				   FROM Members 
-			);
+                   FROM Members );
 
 /* Q7: How can you produce a list of all members who have used a tennis court?
 Include in your output the name of the court, and the name of the member
@@ -64,5 +63,56 @@ JOIN Bookings as B
 JOIN Members as M
 	ON M.memid = B.memid
 ORDER BY M.firstname, M.surname;
+
+/* Q8: How can you produce a list of bookings on the day of 2012-09-14 which
+will cost the member (or guest) more than $30? Remember that guests have
+different costs to members (the listed costs are per half-hour 'slot'), and
+the guest user's ID is always 0. Include in your output the name of the
+facility, the name of the member formatted as a single column, and the cost.
+Order by descending cost, and do not use any subqueries. */
+
+SELECT F.name, CONCAT( M.firstname,  ' ', M.surname ) AS fullname, F.membercost*B.slots as total_cost
+FROM Bookings B
+JOIN Facilities F ON F.facid = B.facid
+JOIN Members M ON B.memid = M.memid
+AND B.memid !=0
+WHERE starttime LIKE  '%2012-09-14%'
+AND F.membercost*B.slots > 30
+UNION ALL 
+SELECT F.name, CONCAT( M.firstname,  ' ', M.surname ) AS fullname, F.guestcost*B.slots as total_cost
+FROM Bookings B
+JOIN Facilities F ON F.facid = B.facid
+JOIN Members M ON B.memid = M.memid
+AND B.memid =0
+WHERE starttime LIKE  '%2012-09-14%'
+AND F.guestcost*B.slots > 30
+ORDER BY fullname
+
+/* Q9: This time, produce the same result as in Q8, but using a subquery. */
+
+SELECT F.name, CONCAT( M.firstname,  ' ', M.surname ) AS fullname, F.membercost*B.slots as total_cost
+FROM (SELECT memid, facid, slots FROM Bookings WHERE starttime like '%2012-09-14%' AND memid != 0) B
+JOIN Facilities F ON F.facid = B.facid
+JOIN Members M ON B.memid = M.memid
+WHERE F.membercost*B.slots > 30
+UNION ALL
+SELECT F.name, CONCAT( M.firstname,  ' ', M.surname ) AS fullname, F.guestcost*B.slots as total_cost
+FROM (SELECT memid, facid, slots FROM Bookings WHERE starttime like '%2012-09-14%' AND memid = 0) B
+JOIN Facilities F ON F.facid = B.facid
+JOIN Members M ON B.memid = M.memid
+WHERE F.guestcost*B.slots > 30
+ORDER BY fullname
+
+/* Q10: Produce a list of facilities with a total revenue less than 1000.
+The output of facility name and total revenue, sorted by revenue. Remember
+that there's a different cost for guests and members! */
+SELECT F.name, SUM(B.slots * (CASE WHEN B.memid > 0 THEN F.membercost
+                        ELSE F.guestcost END) ) AS revenue
+FROM Bookings B
+JOIN Facilities F
+ON F.facid = B.facid
+GROUP BY F.facid
+HAVING revenue < 1000
+ORDER BY revenue
 
 
